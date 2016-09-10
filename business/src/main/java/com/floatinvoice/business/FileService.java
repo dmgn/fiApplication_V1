7d1916c73e6d053d287d65a1d224c3b6cec34b41
dmgn <naikgautamg@gmail.com>
2016-09-10 10:19:26 -0400

    Defect Fixes

--- business/src/main/java/com/floatinvoice/business/FileService.java
+++ business/src/main/java/com/floatinvoice/business/FileService.java
@@ -11,12 +11,18 @@ public interface FileService {
 	ByteMsg download(String refId);
 	
 	ByteMsg downloadSupportDocs(String refId);
+	
+	ByteMsg downloadInvoiceTemplateDocs(String refId);
 
 	ByteMsg downloadLenderAgreement(String lenderAcronym, String refId);
 	
 	BaseMsg uploadLenderAgreement( UploadMessage msg)  throws Exception;
 
+	BaseMsg uploadInvoiceTemplate( UploadMessage msg)  throws Exception;
+	
 	ListMsg<SupportDocDtls> summarySupportDocs(int companyId, int userId); 
 	
 	ListMsg<SupportDocDtls> summarySupportDocs(String acronym); 
+	
+	SupportDocDtls invoiceTemplateMetaData(String category);
 }
