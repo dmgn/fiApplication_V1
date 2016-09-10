6a2a752d47efc09f9aaa7fe7f6355c40377168af
dmgn <naikgautamg@gmail.com>
2016-09-10 10:19:30 -0400

    Defect Fixes

--- common/src/main/java/com/floatinvoice/common/DocCategoryEnum.java
+++ common/src/main/java/com/floatinvoice/common/DocCategoryEnum.java
@@ -1,12 +1,29 @@
 package com.floatinvoice.common;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum DocCategoryEnum {
 
 
 	IDPROOF(1, "IDPROOF"),
 	ADDRESSPROOF(2, "ADDRESSPROOF" ),
 	TAXRETURNS(3, "TAXRETURNS" ),
-	OTHERS(4, "OTHERS");
+	OTHERS(4, "OTHERS"),
+	INVOICETEMPLATE(5,"INVOICETEMPLATE");
+	
+	
+	private static final Map<Integer,DocCategoryEnum> lookup = new HashMap<>();
+
+	static {
+	     for(DocCategoryEnum s : EnumSet.allOf(DocCategoryEnum.class))
+	          lookup.put(s.getCode(), s);
+	}
+	
+	public static DocCategoryEnum get(int code) { 
+	      return lookup.get(code); 
+	}
 	
 	int code;
 	String name;
