89e96d11691103f7720aaf6f5bdd0a82ea759b4d
dmgn <naikgautamg@gmail.com>
2016-09-24 23:12:18 -0400

    Deployed

--- floatinvoice/WebContent/WEB-INF/js/registerApp.js
+++ floatinvoice/WebContent/WEB-INF/js/registerApp.js
@@ -7,12 +7,18 @@
             .accentPalette('green');
         });*/
 
+   registrationApp.service('regUserEmail', function(){
+      this.getRegUserEmail = function(){
+        return "${acronym}";
+      };
+    });
 
 registrationApp.config(function ($locationProvider, $routeProvider, $routeSegmentProvider) {
     $routeSegmentProvider.
       when('/signInInfo','s1').
       when('/companyInfo','s2').
       when('/personalInfo','s3').
+     // when('/createApp','s4').
       when('/documents','s4');
    
     $routeSegmentProvider
@@ -33,6 +39,12 @@ registrationApp.config(function ($locationProvider, $routeProvider, $routeSegmen
           controller: 'RegistrationUsrInfoController'
     });
 
+/* $routeSegmentProvider
+      .segment('s4', {
+          templateUrl: '/floatinvoice/register/chooseProductPage',
+          controller: 'RegistrationProductController'
+    });*/
+
     $routeSegmentProvider
       .segment('s4', {
           templateUrl: '/floatinvoice/register/docs',
@@ -48,16 +60,28 @@ registrationApp.config(function ($locationProvider, $routeProvider, $routeSegmen
 });*/
 
         // create angular controller
-registrationApp.controller('RegistrationController', ['$scope', '$location', function($scope, $location) {
+registrationApp.controller('RegistrationController', ['$scope', '$location', 'regUserEmail', function($scope, $location, regUserEmail) {
 
       $scope.bctabs = [
-        { link : '#/signInInfo', label : 'Sign In Info' },
+        { link : '#/signInInfo', label : 'Change Password' },
         { link : '#/companyInfo', label : 'Company Info' },
-        { link : '#/personalInfo', label : 'Personal Info' },
+        { link : '#/personalInfo', label : 'Director Info' },
+       // { link : '#/createApp', label : 'Choose Product' },
         { link : '#/documents', label : 'Documents' },
         
       ];
       $scope.bcselectedTab = $scope.bctabs[0];   
+
+      var index = -1;
+      var bctabList =  $scope.bctabs;
+      for (var i=0; i<bctabList.length; i++){
+        if( bctabList[i].link == '#'+$location.url() ) {
+            index = i;
+            break;
+        }
+      }
+
+      $scope.bcselectedTab = $scope.bctabs[index];
 
       $scope.bcsetSelectedTab = function(tab) {
         $scope.bcselectedTab = tab;
@@ -68,6 +92,22 @@ registrationApp.controller('RegistrationController', ['$scope', '$location', fun
         } else {
           return "";
         }
+      }
+
+      $scope.getSelectedTab = function(){
+          return $scope.bcselectedTab;
+      }
+
+      $scope.breadCrumbs = function(){
+          var index = -1;
+          var bctabList =  $scope.bctabs;
+          for (var i=0; i<bctabList.length; i++){
+            if( bctabList[i].link == '#'+$location.url() ) {
+                index = i;
+                break;
+            }
+          }
+          $scope.bcselectedTab = $scope.bctabs[index];
       }
 
 }]);
