5ba87b434d942b69ae917957ce433ca04b8f7ac6
dmgn <naikgautamg@gmail.com>
2016-09-24 23:12:11 -0400

    Deployed

--- business/src/main/java/com/floatinvoice/business/dao/EnquiryDao.java
+++ business/src/main/java/com/floatinvoice/business/dao/EnquiryDao.java
@@ -2,6 +2,7 @@ package com.floatinvoice.business.dao;
 
 import java.util.List;
 
+import com.floatinvoice.messages.BaseMsg;
 import com.floatinvoice.messages.EnquiryFormMsg;
 
 public interface EnquiryDao {
@@ -19,4 +20,8 @@ public interface EnquiryDao {
 	int mapEnquiryToOrgSetup(String enquiryRefId, int enquiryId, int orgId, int userId);
 	
 	EnquiryFormMsg viewStagedEnquiry(int enqStatus, String refId, String companyId);
+		
+	BaseMsg qualifyEnquiry(String refId, String email);
+	
+	BaseMsg rejectEnquiry(String refId, String email);
 }
