9372749420e93e7a82549fe0e1e53fe01e3c6523
dmgn <naikgautamg@gmail.com>
2016-09-24 23:12:18 -0400

    Deployed

--- floatinvoice/WebContent/WEB-INF/js/registerOrgInfo.js
+++ floatinvoice/WebContent/WEB-INF/js/registerOrgInfo.js
@@ -1,5 +1,10 @@
   // create angular app
-        var validationApp = angular.module('registrationApp');
+        var validationApp = angular.module('registrationApp')
+          /*.service('fiService', function(){
+              this.getAcronym = function(){
+              return "${acronym}";
+         };
+        })*/
          /*validationApp.config(function($mdThemingProvider) {
           $mdThemingProvider.theme('customTheme')
             .primaryPalette('grey')
@@ -8,6 +13,9 @@
         // create angular controller
         validationApp.controller('RegistrationOrgInfoController', function($scope, $window, $http, $location) {
             $scope.user= {};
+           /* $http.get('/floatinvoice/register/findCorpInfo?acro='+fiService.getAcronym()).success(function(data){
+                $scope.products = data.list;
+            });*/
             $scope.respMsg = "";
             $scope.states = ('Andhra Pradesh,Arunachal Pradesh,Assam,Bihar,Chhattisgarh,Goa,Gujarat,Haryana,Himachal Pradesh,Jammu and Kashmir,Jharkhand,Karnataka,Kerala,Madhya Pradesh,Maharashtra,Manipur,Meghalaya,Mizoram,Nagaland,Orissa,Punjab,Rajasthan,Sikkim,Tamil Nadu,Tripura,Uttarakhand,Uttar Pradesh,West Bengal,Andaman and Nicobar Islands,Chandigarh,Dadra and Nagar Haveli,Daman and Diu,Delhi,Lakshadweep,Pondicherry').split(',').map(function(state) {
             return {abbrev: state};
@@ -16,17 +24,24 @@
             $scope.nextAction = function(){
                 //bcsetSelectedTab(bctabs[1]);
                 $location.path("/personalInfo");
+                $scope.breadCrumbs();
             }
             $scope.checkRespMsg = function(){
 
                 if($scope.respMsg.length > 1){
-                    console.log("returning true");
+                    
                     return true;
                 }else{
-                    console.log("returning false");
+                    
                     return false;
                 }   
             }
+
+            $scope.orgBreadCrumbs = function(){
+                console.log("orgBreadCrumbs");
+                $scope.breadCrumbs();
+            }
+
             // function to submit the form after all validation has occurred            
             $scope.submitForm = function() {
                 // check to make sure the form is completely valid
