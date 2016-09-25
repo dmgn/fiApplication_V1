0779a2f33cee60314e044e90e38d3e599086a4df
dmgn <naikgautamg@gmail.com>
2016-09-24 23:12:13 -0400

    Deployed

--- business/src/main/java/com/floatinvoice/business/dao/JdbcProfileDao.java
+++ business/src/main/java/com/floatinvoice/business/dao/JdbcProfileDao.java
@@ -4,10 +4,12 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.ColumnMapRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import com.floatinvoice.messages.BaseMsg;
 import com.floatinvoice.messages.UserProfile;
 
 public class JdbcProfileDao implements ProfileDao {
@@ -60,8 +62,41 @@ public class JdbcProfileDao implements ProfileDao {
 		final String sql = " SELECT * FROM CLIENT_LOGIN_INFO CLI WHERE CLI.EMAIL = :email";
 		MapSqlParameterSource params = new MapSqlParameterSource();
 		params.addValue("email", userEmail);
-		Map<String, Object> result = jdbcTemplate.queryForObject(sql, params, new ColumnMapRowMapper());
-		return result.size() > 0 ? true : false;
+		Map<String, Object> result = null;
+		try{
+			result = jdbcTemplate.queryForObject(sql, params, new ColumnMapRowMapper());
+		}catch(EmptyResultDataAccessException ex){
+			return false;
+		}
+		return (result != null && result.size() > 0) ? true : false;
+	}
+
+	@Override
+	public BaseMsg deleteUser(String userEmail) {
+		final String sql1 = "SELECT USER_ID FROM CLIENT_LOGIN_INFO WHERE EMAIL = :email"; 
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("email", userEmail);
+		Map<String, Object> result = jdbcTemplate.queryForObject(sql1, params, new ColumnMapRowMapper());
+		int userId = (int)result.get("USER_ID");
+		params.addValue("userId", userId);
+		final String selectSMEUser = "SELECT COMPANY_ID FROM USER_ORGANIZATION_MAP UOM WHERE UOM.USER_ID = :userId";
+		Map<String, Object> resultUserId = jdbcTemplate.queryForObject(selectSMEUser, params, new ColumnMapRowMapper());
+		int companyId = (int)resultUserId.get("COMPANY_ID");
+		params.addValue("companyId", companyId);
+		final String deleteUser = "DELETE FROM USER_ORGANIZATION_MAP UOM WHERE UOM.USER_ID = :userId";
+		jdbcTemplate.queryForObject(deleteUser, params, new ColumnMapRowMapper());
+		
+		final String deleteUser2 = "DELETE FROM SME_USER_INFO SUI WHERE SUI.USER_ID = :userId";
+		jdbcTemplate.queryForObject(deleteUser2, params, new ColumnMapRowMapper());
+		
+		final String deleteUser3 = "DELETE FROM CLIENT_LOGIN_INFO CLI WHERE CLI.USER_ID = :userId";
+		jdbcTemplate.queryForObject(deleteUser3, params, new ColumnMapRowMapper());
+		
+		final String deleteOrg = "DELETE FROM ORGANIZATION_INFO OI WHERE OI.COMPANY_ID = :companyId";
+		jdbcTemplate.queryForObject(deleteOrg, params, new ColumnMapRowMapper());
+		BaseMsg baseMsg = new BaseMsg();
+		baseMsg.addInfoMsg("Deleted successfully", 0);
+		return baseMsg;
 	}
 
 	/*@Override
