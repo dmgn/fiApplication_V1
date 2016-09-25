394ec3aa3d261af8f7b352800900e127d0b33998
dmgn <naikgautamg@gmail.com>
2016-09-24 23:12:14 -0400

    Deployed

--- business/src/main/java/com/floatinvoice/business/dao/RegistrationDao.java
+++ business/src/main/java/com/floatinvoice/business/dao/RegistrationDao.java
@@ -2,6 +2,7 @@ package com.floatinvoice.business.dao;
 
 import com.floatinvoice.messages.BaseMsg;
 import com.floatinvoice.messages.ListMsg;
+import com.floatinvoice.messages.RegistrationStep1SignInDtlsMsg;
 import com.floatinvoice.messages.RegistrationStep2CorpDtlsMsg;
 import com.floatinvoice.messages.RegistrationStep3UserPersonalDtlsMsg;
 import com.floatinvoice.messages.SupportDocDtls;
@@ -13,11 +14,15 @@ public interface RegistrationDao {
 	
 	BaseMsg registerOrgInfo(RegistrationStep2CorpDtlsMsg msg);
 
+	RegistrationStep2CorpDtlsMsg fetchOrgInfo(String acronym);
+	
 	BaseMsg registerUserBankInfo (RegistrationStep3UserPersonalDtlsMsg msg);
+	
+	RegistrationStep3UserPersonalDtlsMsg fetchUserBankInfo(String acronym);
 	
 	BaseMsg fileUpload(UploadMessage msg) throws Exception;
 	
 	ListMsg<SupportDocDtls> summary(String acronym);
 
-	
+	BaseMsg updateRegisteredEmail(RegistrationStep1SignInDtlsMsg msg);
 }
