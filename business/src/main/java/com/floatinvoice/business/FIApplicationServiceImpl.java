49805a7df238324dba37775ef7f5eaf4f323d5f2
dmgn <naikgautamg@gmail.com>
2016-11-08 23:53:33 -0500

    version Deal Origination

--- business/src/main/java/com/floatinvoice/business/FIApplicationServiceImpl.java
+++ business/src/main/java/com/floatinvoice/business/FIApplicationServiceImpl.java
@@ -7,10 +7,15 @@ import com.floatinvoice.business.dao.EnquiryDao;
 import com.floatinvoice.business.dao.FIApplicationDao;
 import com.floatinvoice.business.dao.FIProductDao;
 import com.floatinvoice.business.dao.OrgReadDao;
+import com.floatinvoice.common.EnquiryStatusEnum;
 import com.floatinvoice.common.UserContext;
 import com.floatinvoice.messages.AppDtlsMsg;
 import com.floatinvoice.messages.BaseMsg;
+import com.floatinvoice.messages.EnquiryFormMsg;
+import com.floatinvoice.messages.KYCDtlsMsg;
 import com.floatinvoice.messages.ProductDtls;
+import com.floatinvoice.messages.RegistrationStep2CorpDtlsMsg;
+import com.floatinvoice.messages.RegistrationStep3UserPersonalDtlsMsg;
 
 public class FIApplicationServiceImpl implements FIApplicationService {
 
@@ -18,17 +23,28 @@ public class FIApplicationServiceImpl implements FIApplicationService {
 	OrgReadDao orgReadDao;
 	FIApplicationDao fiApplicationDao;
 	FIProductDao fiProductDao;
+	RegistrationService regService;
+	BankService bankService;
+	EnquiryService enquiryService;
+	
+	
 	
 	public FIApplicationServiceImpl(){
 		
 	}
 	
 	public FIApplicationServiceImpl( EnquiryDao enquiryDao, OrgReadDao orgReadDao, FIApplicationDao fiApplicationDao,
-			FIProductDao fiProductDao){
+			FIProductDao fiProductDao,
+			RegistrationService regService,
+			BankService bankService,
+			EnquiryService enquiryService){
 		this.enquiryDao = enquiryDao;
 		this.orgReadDao = orgReadDao;
 		this.fiApplicationDao = fiApplicationDao;
 		this.fiProductDao = fiProductDao;
+		this.regService = regService;
+		this.bankService = bankService;
+		this.enquiryService = enquiryService;
 	}
 	
 	@Override
@@ -56,6 +72,22 @@ public class FIApplicationServiceImpl implements FIApplicationService {
 	@Override
 	public BaseMsg editApplication(AppDtlsMsg appDetails) {
 		return null;
+	}
+
+	@Override
+	public KYCDtlsMsg viewOneKYCApplication(String acro, String enqRefId) {
+		KYCDtlsMsg response = new KYCDtlsMsg();
+		RegistrationStep2CorpDtlsMsg regOffice = regService.fetchOrgInfo(acro);
+		response.setRegOffice(regOffice);
+		RegistrationStep3UserPersonalDtlsMsg bankDtls = regService.fetchUserBankInfo(acro);
+		response.setBankDtls(bankDtls);
+		EnquiryFormMsg enquiry = enquiryService.viewOneEnquiry(enqRefId);
+		response.setContactPersonContactNo(enquiry.getPhone());
+		response.setContactPersonEmail(enquiry.getEmail());
+		response.setContactPersonName(enquiry.getContactName());
+		response.setRegCompName(regOffice.getCompName());
+		//response.setCo
+		return response;
 	}
 
 }
