371c42be81a6be89104ebb708414376107ba3e8f
dmgn <naikgautamg@gmail.com>
2016-11-08 23:53:32 -0500

    version Deal Origination

--- business/src/main/java/com/floatinvoice/business/FIApplicationService.java
+++ business/src/main/java/com/floatinvoice/business/FIApplicationService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.floatinvoice.messages.AppDtlsMsg;
 import com.floatinvoice.messages.BaseMsg;
+import com.floatinvoice.messages.KYCDtlsMsg;
 
 
 public interface FIApplicationService {
@@ -12,4 +13,6 @@ public interface FIApplicationService {
 	AppDtlsMsg viewOneApplication(String refId);
 	BaseMsg saveApplication( AppDtlsMsg appDetails );
 	BaseMsg editApplication( AppDtlsMsg appDetails );
+	
+	KYCDtlsMsg viewOneKYCApplication(String acro, String regId);
 }
