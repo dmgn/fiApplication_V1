484bb4c7c596e08d9765d110720d827f12be236c
dmgn <naikgautamg@gmail.com>
2016-11-22 14:00:28 -0500

    New changes

--- business/src/main/java/com/floatinvoice/business/FIApplicationServiceImpl.java
+++ business/src/main/java/com/floatinvoice/business/FIApplicationServiceImpl.java
@@ -7,46 +7,40 @@ import com.floatinvoice.business.dao.EnquiryDao;
 import com.floatinvoice.business.dao.FIApplicationDao;
 import com.floatinvoice.business.dao.FIProductDao;
 import com.floatinvoice.business.dao.OrgReadDao;
-import com.floatinvoice.common.EnquiryStatusEnum;
 import com.floatinvoice.common.UserContext;
 import com.floatinvoice.messages.AppDtlsMsg;
 import com.floatinvoice.messages.BaseMsg;
-import com.floatinvoice.messages.EnquiryFormMsg;
+import com.floatinvoice.messages.BuyerDtlsMsg;
 import com.floatinvoice.messages.KYCDtlsMsg;
+import com.floatinvoice.messages.ListMsg;
 import com.floatinvoice.messages.ProductDtls;
-import com.floatinvoice.messages.RegistrationStep2CorpDtlsMsg;
-import com.floatinvoice.messages.RegistrationStep3UserPersonalDtlsMsg;
 
-public class FIApplicationServiceImpl implements FIApplicationService {
+public class FIApplicationServiceImpl implements FIApplicationService{
 
 	EnquiryDao enquiryDao;
 	OrgReadDao orgReadDao;
 	FIApplicationDao fiApplicationDao;
 	FIProductDao fiProductDao;
-	RegistrationService regService;
+	RegistrationService registrationService;
 	BankService bankService;
 	EnquiryService enquiryService;
-	
-	
 	
 	public FIApplicationServiceImpl(){
 		
 	}
 	
-	public FIApplicationServiceImpl( EnquiryDao enquiryDao, OrgReadDao orgReadDao, FIApplicationDao fiApplicationDao,
-			FIProductDao fiProductDao,
-			RegistrationService regService,
-			BankService bankService,
-			EnquiryService enquiryService){
+	public FIApplicationServiceImpl(EnquiryDao enquiryDao, OrgReadDao orgReadDao, FIApplicationDao fiApplicationDao,
+			FIProductDao fiProductDao, RegistrationService registrationService,
+			BankService bankService, EnquiryService enquiryService){
 		this.enquiryDao = enquiryDao;
 		this.orgReadDao = orgReadDao;
 		this.fiApplicationDao = fiApplicationDao;
 		this.fiProductDao = fiProductDao;
-		this.regService = regService;
+		this.registrationService = registrationService;
 		this.bankService = bankService;
 		this.enquiryService = enquiryService;
 	}
-	
+
 	@Override
 	public List<AppDtlsMsg> viewAllApplications(String acro) {
 		// TODO Auto-generated method stub
@@ -61,33 +55,36 @@ public class FIApplicationServiceImpl implements FIApplicationService {
 
 	@Override
 	public BaseMsg saveApplication(AppDtlsMsg appDetails) {
+		Map<String, Object> org = orgReadDao.findOrgId(appDetails.getAcro());
+		ProductDtls productDtls = fiProductDao.findProductByRefId(appDetails.getProductRefId());
+		int companyId = (int) org.get("company_id");
 		Map<String, Object> orgInfo = orgReadDao.findOrgAndUserId(UserContext.getUserName());
-		ProductDtls productDtls = fiProductDao.findProductByRefId(appDetails.getRefId());
-		return fiApplicationDao.saveApplication(appDetails, (Integer)orgInfo.get("COMPANY_ID"), 
-				(Integer)orgInfo.get("USER_ID"), productDtls.getProductId());
-		
-
+		return fiApplicationDao.saveApplication(appDetails, companyId, (int)orgInfo.get("USER_ID"), productDtls.getProductId());
 	}
 
 	@Override
 	public BaseMsg editApplication(AppDtlsMsg appDetails) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public KYCDtlsMsg viewOneKYCApplication(String acro, String enqRefId) {
-		KYCDtlsMsg response = new KYCDtlsMsg();
-		RegistrationStep2CorpDtlsMsg regOffice = regService.fetchOrgInfo(acro);
-		response.setRegOffice(regOffice);
-		RegistrationStep3UserPersonalDtlsMsg bankDtls = regService.fetchUserBankInfo(acro);
-		response.setBankDtls(bankDtls);
-		EnquiryFormMsg enquiry = enquiryService.viewOneEnquiry(enqRefId);
-		response.setContactPersonContactNo(enquiry.getPhone());
-		response.setContactPersonEmail(enquiry.getEmail());
-		response.setContactPersonName(enquiry.getContactName());
-		response.setRegCompName(regOffice.getCompName());
-		//response.setCo
-		return response;
+	public KYCDtlsMsg viewOneKYCApplication(String acro, String refId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BaseMsg saveBuyerDetails(BuyerDtlsMsg buyerDtls) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ListMsg<BuyerDtlsMsg> viewBuyerDetails(String acronym,
+			String appRefId) {
+		
+		return null;
 	}
 
 }
